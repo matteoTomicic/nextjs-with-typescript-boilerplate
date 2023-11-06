e4a42f0126d7be1f0dc9b023e1141eb237dfb239
matteoTomicic <tomicic.matteo7@gmail.com>
2023-11-06 12:18:01 +0100

    chore(pm2): implement pm2

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
 instalirati prettier
 razmilsiti o tome da se uzmeni neki airbnb ili fb eslint config
-pm2 ecosystem
 deploy to prod skripta
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://example.com',
-    generateRobotsTxt: true,
     generateIndexSitemap: false,
+    generateRobotsTxt: true,
+    siteUrl: 'https://example.com',
     // ...other options
 };
